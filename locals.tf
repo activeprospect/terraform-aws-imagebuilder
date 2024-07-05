11ccec562ff70d340cc770dc4890ddf8d2a7475d
Daniel Gershin <46380031+dangershin@users.noreply.github.com>
2024-07-05 14:42:49 +0200

    Fix list

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
   custom_policy_arns = var.imgb_custom_policy != "" && length(aws_iam_policy.custom) > 0 ? [aws_iam_policy.custom[0].arn] : []
-  all_managed_policies = concat(var.imgb_managed_policies, local.custom_policy_arns)
+  managed_policies_list = tolist(var.imgb_managed_policies)
+  all_managed_policies = concat(local.managed_policies_list, local.custom_policy_arns)
 }
